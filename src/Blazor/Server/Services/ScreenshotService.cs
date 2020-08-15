7634c45396539e64844992da07ff8c48192f3a2a
Alex Yakunin <alex.yakunin@gmail.com>
2020-08-15 07:41:52 -0700

    feat: Make ScreenshotService.cs produce frames a bit faster.

--- src/Blazor/Server/Services/ScreenshotService.cs
+++ src/Blazor/Server/Services/ScreenshotService.cs
@@ -15,10 +15,13 @@ namespace Samples.Blazor.Server.Services
     [ComputeService(typeof(IScreenshotService))]
     public class ScreenshotService : IScreenshotService
     {
+        public const int MinWidth = 8;
+        public const int MaxWidth = 1280;
+
         private readonly ImageCodecInfo _jpegEncoder;
         private readonly EncoderParameters _jpegEncoderParameters;
         private readonly Rectangle _displayDimensions;
-        private Task<Bitmap> _next = null!;
+        private Task<(Screenshot, Bitmap)> _next = null!;
 
         public ScreenshotService()
         {
@@ -26,7 +29,7 @@ namespace Samples.Blazor.Server.Services
                 .GetImageDecoders()
                 .Single(codec => codec.FormatID == ImageFormat.Jpeg.Guid);
             _jpegEncoderParameters = new EncoderParameters(1) {
-                Param = {[0] = new EncoderParameter(Encoder.Quality, 50L)}
+                Param = new [] {new EncoderParameter(Encoder.Quality, 50L)}
             };
             _displayDimensions = DisplayInfo.PrimaryDisplayDimensions
                 ?? new Rectangle(0, 0, 1920, 1080);
@@ -34,46 +37,62 @@ namespace Samples.Blazor.Server.Services
 
         public virtual async Task<Screenshot> GetScreenshotAsync(int width, CancellationToken cancellationToken = default)
         {
-            var bScreen = await GetScreenshotAsync(cancellationToken).ConfigureAwait(false);
+            var (screenshot, bitmap) = await GetScreenshotAsync(cancellationToken).ConfigureAwait(false);
+            var w = Math.Min(MaxWidth, Math.Max(MinWidth, width));
+            var h = w * screenshot.Height / screenshot.Width;
+            if (screenshot.Width == w)
+                return screenshot;
+
             // The code below scales a full-resolution screenshot to a desirable resolution
-            var (w, h) = (_displayDimensions.Width, _displayDimensions.Height);
-            var ow = width;
-            var oh = h * ow / w;
-            using var bOut = new Bitmap(ow, oh);
-            using var gOut = Graphics.FromImage(bOut);
-            gOut.CompositingQuality = CompositingQuality.HighSpeed;
-            gOut.InterpolationMode = InterpolationMode.Default;
-            gOut.CompositingMode = CompositingMode.SourceCopy;
-            gOut.DrawImage(bScreen, 0, 0, ow, oh);
-            await using var stream = new MemoryStream();
-            bOut.Save(stream, _jpegEncoder, _jpegEncoderParameters);
-            var bytes = stream.ToArray();
-            var base64Content = Convert.ToBase64String(bytes);
-            return new Screenshot(ow, oh, base64Content);
+            using var bOut = new Bitmap(w, h);
+            Scale(bitmap, bOut);
+            return Encode(bOut);
         }
 
         [ComputeMethod(AutoInvalidateTime = 0.01)]
-        protected virtual Task<Bitmap> GetScreenshotAsync(CancellationToken cancellationToken = default)
+        protected virtual Task<(Screenshot, Bitmap)> GetScreenshotAsync(CancellationToken cancellationToken = default)
         {
             // Captures a full-resolution screenshot; the code here is optimized
             // to produce the next screeenshot in advance.
-            Task<Bitmap> Capture() => Task.Run(() => {
+            Task<(Screenshot, Bitmap)> Capture() => Task.Run(() => {
                 var (w, h) = (_displayDimensions.Width, _displayDimensions.Height);
-                var bScreen = new Bitmap(w, h);
+                using var bScreen = new Bitmap(w, h);
                 using var gScreen = Graphics.FromImage(bScreen);
                 gScreen.CopyFromScreen(0, 0, 0, 0, bScreen.Size);
-                return bScreen;
+                if (w > MaxWidth)
+                    (h, w) = (MaxWidth * h / w, MaxWidth);
+                var bOut = new Bitmap(w, h);
+                Scale(bScreen, bOut);
+                return (Encode(bOut), bOut);
             }, default);
 
             var current = Capture();
             var prev = Interlocked.Exchange(ref _next, current) ?? current;
             Computed.GetCurrent()!.Invalidated += c => Task.Delay(2000).ContinueWith(_ => {
                 // Let's dispose these values in 2 seconds
-                var computed = (IComputed<Bitmap>) c;
+                var computed = (IComputed<(Screenshot, Bitmap)>) c;
                 if (computed.HasValue)
-                    computed.Value.Dispose();
+                    computed.Value.Item2.Dispose();
             });
             return prev;
+        }
+
+        private static void Scale(Bitmap source, Bitmap target)
+        {
+            using var g = Graphics.FromImage(target);
+            g.CompositingQuality = CompositingQuality.HighSpeed;
+            g.InterpolationMode = InterpolationMode.Bilinear;
+            g.CompositingMode = CompositingMode.SourceCopy;
+            g.DrawImage(source, 0, 0, target.Width, target.Height);
+        }
+
+        private Screenshot Encode(Bitmap bitmap)
+        {
+            using var stream = new MemoryStream();
+            bitmap.Save(stream, _jpegEncoder, _jpegEncoderParameters);
+            var bytes = stream.ToArray();
+            var base64Content = Convert.ToBase64String(bytes);
+            return new Screenshot(bitmap.Width, bitmap.Height, base64Content);
         }
     }
 }
