15a358481bc502310fe025008ef995ea3a61b350
Alex Yakunin <alex.yakunin@gmail.com>
2020-09-21 05:21:25 -0700

    fix: Minor fix.

--- src/Blazor/Client/wwwroot/js/auth.js
+++ src/Blazor/Client/wwwroot/js/auth.js
@@ -4,7 +4,7 @@ let openAuthWindow = function (action, flowName) {
     let encode = encodeURIComponent;
     let redirectUrl = new URL("/close?flow=" + encode(flowName), document.baseURI).href;
     let url = "/" + action + "?returnUrl=" + encode(redirectUrl);
-    return window.open(url, "_blank", "width=600,height=600;left=80;top=80");
+    return window.open(url, "_blank", "width=600,height=600");
 }
 
 window.Auth = {
