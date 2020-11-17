33b5b506938cfc28c61fccabb8883e4de2d61b8b
Alex Yakunin <alex.yakunin@gmail.com>
2020-11-16 16:39:18 -0800

    feat: .NET Core 3.1 target for Caching sample + .sln for every sample.

--- src/Caching/Server/Startup.cs
+++ src/Caching/Server/Startup.cs
@@ -8,9 +8,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
-using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
-using Samples.Caching.Common;
 using Samples.Caching.Server.Services;
 using Stl.DependencyInjection;
 using Stl.Fusion;
@@ -65,13 +62,6 @@ namespace Samples.Caching.Server
 
             services.AddRouting();
             services.AddMvc().AddApplicationPart(Assembly.GetExecutingAssembly());
-
-            // Swagger & debug tools
-            services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new OpenApiInfo {
-                    Title = "Samples.Caching.Server API", Version = "v1"
-                });
-            });
         }
 
         public void Configure(IApplicationBuilder app, ILogger<Startup> log)
@@ -92,10 +82,6 @@ namespace Samples.Caching.Server
 
             // Static + Swagger
             app.UseStaticFiles();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
-            });
 
             // API controllers
             app.UseRouting();
