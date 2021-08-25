61b43d5e448ea6504151868752a56cc9ee990917
Alex Yakunin <alex.yakunin@gmail.com>
2021-08-25 10:46:32 -0700

    chore: minor change

--- src/HelloBlazorServer/Services/CounterService.cs
+++ src/HelloBlazorServer/Services/CounterService.cs
@@ -6,7 +6,7 @@ namespace Samples.HelloBlazorServer.Services
 {
     public class CounterService
     {
-        private readonly object _lock = new object();
+        private readonly object _lock = new();
         private int _count;
         private DateTime _changeTime = DateTime.Now;
 
