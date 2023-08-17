a1180f4dce9cb7544a6f1f5580ee55d02e0e92ff
Alex Yakunin <alex.yakunin@gmail.com>
2023-08-16 20:50:10 -0700

    chore: Run-RpcBenchmark-Server.cmd should bind to 0.0.0.0

--- Run-RpcBenchmark-Server.cmd
+++ Run-RpcBenchmark-Server.cmd
@@ -3,6 +3,6 @@
 set serverCoreCount=6
 if NOT "%1"=="" set serverCoreCount=%1
 
-start cmd /C dotnet run -c Release --project src/RpcBenchmark/RpcBenchmark.csproj -- server
+start cmd /C dotnet run -c Release --project src/RpcBenchmark/RpcBenchmark.csproj -- server https://0.0.0.0:22444/
 timeout 3
 powershell.exe -File SetAffinity.ps1 Samples.RpcBenchmark %serverCoreCount%
