a8e8b7f40d7aca8e8170344c57566a58425fc9b3
Alex Yakunin <alex.yakunin@gmail.com>
2023-12-06 08:38:18 -0800

    chore: tweak Rpc streaming settings

--- src/RpcBenchmark/Server/TestService.cs
+++ src/RpcBenchmark/Server/TestService.cs
@@ -16,8 +16,8 @@ public class TestService : ITestService
     public Task<RpcStream<Item>> GetItems(GetItemsRequest request, CancellationToken cancellationToken = default)
     {
         var stream = new RpcStream<Item>(StreamGenerator.GetItems(request, cancellationToken)) {
-            AckPeriod = 2000,
-            AckAdvance = 5001,
+            AckPeriod = 1000,
+            AckAdvance = 3001,
         };
         return Task.FromResult(stream);
     }
